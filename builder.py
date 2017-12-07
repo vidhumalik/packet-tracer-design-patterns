e93be08e856661ee40f4b75bd7b187fa7c1241d1
srinidhidodo <srinidhi.nps@gmail.com>
2017-12-07 22:11:05 +0530

    Ping functionality working
    Redirect input to inputs1.txt

--- builder.py
+++ builder.py
@@ -1,6 +1,126 @@
 #import flyweight
 
-#flyweightFactory = FlyweightFactory()
+currObjs = []
+adjMat = {}
+IPList = []
+'''
+Ping working:
+Main class keeps a list of objects where ping is at the moment, and runs send function on all of them.
+The send function will update the list of current objects.
+'''
+
+class FlyweightFactory:
+    """
+    Create and manage flyweight objects.
+    Ensure that flyweights are shared properly. When a client requests a
+    flyweight, the FlyweightFactory object supplies an existing instance
+    or creates one, if none exists.
+    """
+
+    def __init__(self):
+        self._flyweights = {'PC':PCFlyweight(),'Hub':HubFlyweight(),'Router':RouterFlyweight()}
+
+    def get_flyweight(self, key):
+        try:
+            flyweight = self._flyweights[key]
+        except KeyError:
+            flyweight = ConcreteFlyweight()
+            self._flyweights[key] = flyweight
+        return flyweight
+
+
+class Flyweight():
+    def __init__(self):
+        self.intrinsic_state = None
+
+    def receive(self, extrinsic_state, ping):
+        '''
+        Ping is a dictionary for ping details, like {destinationIP:__, sourceIP:__, visitedObjs:[]...}
+        Similarly, extrinsic_state={IP:__, ...}
+        Return value: True for successful ping, False otherwise
+        '''
+        pass
+    
+    def send(self, extrinsic_state, ping):
+        pass
+
+
+class ConcreteFlyweight(Flyweight):
+    def receive(self, extrinsic_state, ping):
+        pass
+    def send(self, extrinsic_state, ping):
+        pass
+
+class PCFlyweight(Flyweight):
+    '''
+    Calls actually will be like PC.send(ping) or PC.receive(ping) to PC class.
+    PC class will call the flyweight version of the functions, passing the extrinsic_state.
+    Same for hub and router classes
+    '''
+    def receive(self, extrinsic_state, ping):
+        '''
+        print('In PC')
+        print(ping)
+        print(extrinsic_state)
+        print('-----')
+        '''
+        if extrinsic_state['IP'] == ping['destinationIP']:
+            return True
+        return False
+    def send(self, extrinsic_state, ping):
+        forwardToObj = adjMat['byIp'][extrinsic_state['IP']]
+        return [forwardToObj]
+
+class HubFlyweight(Flyweight):
+    def receive(self, extrinsic_state, ping): #pass, because hub doesnt perform any processing based on the ping, as PC needs to
+        pass
+    def send(self, extrinsic_state, ping): #Returns list of objects that hub will forward the ping to
+        '''
+        print('in Hub')
+        print(self)
+        print('-----')
+        '''
+        forwardToObjList = adjMat['byObj'][extrinsic_state['Hub']]
+        return forwardToObjList
+
+class RouterFlyweight(Flyweight):
+    '''
+    Routing table sample (extrinsic_state['routingTable'])
+    {destIP:portNumberOfRouter, destIP:portNumberOfRouter, ...}
+    
+    Sample extrinsic_state['ports'] = {portNumberOfRouter: IPofPort, portNumberOfRouter: IPofPort, ...}
+    
+    '''
+    def receive(self, extrinsic_state, ping): #pass, because router doesnt perform any processing based on the ping, as PC needs to
+        pass
+    def send(self, extrinsic_state, ping): #Returns object that router will forward the ping to, as a list
+        '''
+        portOfRouter = extrinsic_state['routingTable'][ping['destinationIP']]
+        IPofRouterPort = extrinsic_state['ports'][portOfRouter]
+        forwardToObj = adjMat['byIp'][IPofRouterPort]
+        '''
+        portOfRouter = -1
+        forwardToObj = None
+        routingTable = extrinsic_state['routingTable']
+        for route in routingTable:
+            #if route.network.network.ipaddress[:route.zeroFrom] == ping['destinationIP'][:route.zeroFrom]:
+            if route['network'][:route['zeroFrom']] == ping['destinationIP'][:route['zeroFrom']]:
+                #to forward on this route
+                #routerPortIp = route.nexthop.gateway.ipaddress
+                forwardToObj = adjMat['byIp'][route['nexthop']]
+                break
+        return [forwardToObj]
+
+
+
+
+
+
+
+
+
+
+flyweightFactory = FlyweightFactory()
 
 class Director:
     
@@ -17,7 +137,7 @@ class Director:
 
     # The algorithm for assembling a car
     def createComponent(self, name = None, gateway = None,ipaddress = None,subnetmask = None,dnsserver = None, routes = None, fastethernet = None ):
-        print(name,gateway, ipaddress, subnetmask, dnsserver, routes, fastethernet)
+        #print(name,gateway, ipaddress, subnetmask, dnsserver, routes, fastethernet)
         #component = Component()
         #self.__builder.getComponentType()
         self.__builder.addName(name)
@@ -41,6 +161,7 @@ class Director:
         self.__builder.addFastEthernet(fastethernet)
         #component.setFastEthernet(fastethernet)
 
+        self.__builder.getFlyweight()
         #component.setFlyweight()
 
         return self.__builder.getComponent()
@@ -96,6 +217,10 @@ class Component:
         #print "tire size: %d\'" % self.__wheels[0].size
     def getType(self):
         pass
+    def send(self,ping):
+        pass
+    def receive(self,ping):
+        pass
 
 class Router(Component):
 
@@ -132,6 +257,11 @@ class Router(Component):
     def getType(self):
         return 'Router'
 
+    def send(self,ping):
+        return self.__flyweight.send({'routingTable':self.getRoutes()}, ping)
+
+    def receive(self,ping):
+        return self.__flyweight.receive({'routingTable':self.getRoutes()}, ping)
 
 class Hub(Component):
 
@@ -152,6 +282,12 @@ class Hub(Component):
 
     def getType(self):
         return 'Hub'
+
+    def send(self,ping):
+        return self.__flyweight.send({'Hub':self}, ping)
+
+    def receive(self,ping):
+        return self.__flyweight.receive({'Hub':self}, ping)
         
 class PC(Component):
 
@@ -205,6 +341,12 @@ class PC(Component):
     def getType(self):
         return 'PC'
 
+    def send(self,ping):
+        return self.__flyweight.send({'IP':self.getIPAddress()}, ping)
+
+    def receive(self,ping):
+        return self.__flyweight.receive({'IP':self.getIPAddress()}, ping)
+
 
 class Builder:
 
@@ -223,6 +365,7 @@ class Builder:
     def addFastEthernet(self, arg_fastethernet): pass
     def addFlyweight(self): pass
     def getComponent(self): pass
+    def getFlyweight(self): pass
 
 class HubBuilder(Builder):
 
@@ -237,6 +380,8 @@ class HubBuilder(Builder):
 
     def addName(self, arg_name):
         self.myhub.setName(arg_name)
+    def getFlyweight(self):
+        self.myhub.setFlyweight()
     def getComponent(self):
         return self.myhub
 
@@ -263,6 +408,8 @@ class PCBuilder(Builder):
        # return dns
     def addName(self,arg_name):
         self.myPC.setName(arg_name)
+    def getFlyweight(self):
+        self.myPC.setFlyweight()
     def getComponent(self):
         return self.myPC
 
@@ -286,6 +433,9 @@ class RouterBuilder(Builder):
     def addFastEthernet(self, arg_fastethernet):
 #4 ethernets, each has mac, ip address, subnet mask
         self.myrouter.setFastEthernet(arg_fastethernet)
+
+    def getFlyweight(self):
+        self.myrouter.setFlyweight()
     
     def getComponent(self):
         return self.myrouter
@@ -322,12 +472,78 @@ class DNSServer:
 class fourPartAddress:
     pass
 
+def inputIp(toEnter):
+    valid = True
+    str1 = 'Please enter '+toEnter+': '
+    str2 = 'Invalid entry. Please re-enter '+toEnter+': '
+    inp = raw_input(str1)
+    while True:
+        checker = inp.split('.')
+        valid = True
+        if len(checker) != 4 or inp in IPList:
+            valid = False
+        for i in checker:
+            if len(i)==0 or int(i)<0 or int(i)>255:
+                valid = False
+        if valid == True:
+            IPList.append(inp)
+            return inp
+        inp = raw_input(str2)
+
+def inputIpForRoutingTable(toEnter):
+    valid = True
+    str1 = 'Please enter '+toEnter+': '
+    str2 = 'Invalid entry. Please re-enter '+toEnter+': '
+    inp = raw_input(str1)
+    while True:
+        checker = inp.split('.')
+        valid = True
+        if len(checker) != 4:
+            valid = False
+        for i in checker:
+            if len(i)==0 or int(i)<0 or int(i)>255:
+                valid = False
+        if valid == True:
+            return inp
+        inp = raw_input(str2)
+
+def inputIpExisting(toEnter):
+    valid = True
+    str1 = 'Please enter '+toEnter+': '
+    str2 = 'Invalid entry. Please re-enter '+toEnter+': '
+    inp = raw_input(str1)
+    while True:
+        checker = inp.split('.')
+        valid = True
+        if len(checker) != 4 or inp not in IPList:
+            valid = False
+        for i in checker:
+            if len(i)==0 or int(i)<0 or int(i)>255:
+                valid = False
+        if valid == True:
+            return inp
+        inp = raw_input(str2)
+
+def inputName():
+    inp = raw_input('Enter name: ')
+    while True:
+        if inp not in adjMat['byName']:
+            return inp
+        inp = raw_input('Name already exists. Please re-enter unique name: ')
+
+def checkName(msg):
+    inp = raw_input(msg)
+    while True:
+        if inp in adjMat['byName'] or inp=='0':
+            return inp
+        inp = raw_input('Object does not exist. Enter existing object name: ')
+
 
 def main():
     director = Director()
     tostop = 0
     isfirst=1
-    adjMat = {}
+    #adjMat = {}
     adjMat["byName"] = {}
     adjMat["byObj"] = {}
     adjMat['byIp'] = {}
@@ -343,11 +559,11 @@ def main():
                 choice2=int(raw_input("1. PC \n2. Hub \n3. Router\n"))
                 if(choice2 == 1):
                     print("Enter the following details:")
-                    name = raw_input("Enter Name: ")
-                    ipaddress = raw_input("Enter Ip Address: ")
-                    subnetmask = raw_input("Enter Subnet Mask: ")
-                    gateway = raw_input("Enter Gateway: ")
-                    dnsserver = raw_input("Enter DNS Server: ")
+                    name = inputName() #raw_input("Enter Name: ")
+                    ipaddress = inputIp('IP address')
+                    subnetmask = raw_input("Enter Subnet Mask: ") #inputIp('subnet mask')
+                    gateway = raw_input("Enter Gateway: ") #inputIp('gateway')
+                    dnsserver = raw_input("Enter DNS Server: ") #inputIp('DNS server')
                     director.setBuilder(PCBuilder())
                     pc = director.createComponent(name = name, ipaddress = ipaddress, subnetmask = subnetmask, gateway = gateway, dnsserver = dnsserver)
                     adjMat["byName"][name] = pc
@@ -355,7 +571,7 @@ def main():
                     print(list(adjMat["byName"].keys()))
                     print("Enter the names of objects that this given PC connects to. To stop entering links, enter 0")
                     
-                    linkChoice = raw_input()
+                    linkChoice = checkName('') #raw_input()
                     while(linkChoice != '0'):
                         #ensure that linkchoice is valid
                         tempObj = adjMat["byName"][linkChoice]
@@ -379,20 +595,19 @@ def main():
                             adjMat['byIp'][ipOfRouter] = pc
 
                         #invalid link check
-                        linkChoice = raw_input()
+                        linkChoice = checkName('') #raw_input()
                     pc.specification()
 
                 elif(choice2 == 2):
                     print("Enter the following details:")
-                    name = raw_input("Enter Name: ")
+                    name = inputName() #raw_input("Enter Name: ")
                     director.setBuilder(HubBuilder())
                     hub = director.createComponent(name = name)
                     adjMat["byName"][name] = hub
                     print("The current list of objects that exists is as follows:")
                     print(list(adjMat["byName"].keys()))
-                    print(adjMat['byName'])
                     print("Enter the names of objects that this given Hub connects to. To stop entering links, enter 0")
-                    linkChoice = raw_input()
+                    linkChoice = checkName('') #raw_input()
                     while(linkChoice != '0'):
                         #ensure that linkchoice is valid
                         tempObj = adjMat["byName"][linkChoice]
@@ -415,11 +630,11 @@ def main():
                             adjMat['byIp'][ipOfRouter] = hub
 
                         #invalid link check
-                        linkChoice = raw_input()
+                        linkChoice = checkName('') #raw_input()
                     hub.specification()
                 elif(choice2 == 3):
                     print("Enter the following details:")
-                    name = raw_input("Enter Name: ")
+                    name = inputName() #raw_input("Enter Name: ")
                     print("Enter the details pertaining to the ethernet ports. Maximum number of ports in a router is 4")
                     count= 1
                     tostop2 = 0
@@ -436,8 +651,8 @@ def main():
                         if(choice3 != '0'):
                             temp = {}
                             temp["mac"] = raw_input("Enter mac address: ")
-                            temp["ipaddress"] = raw_input("Enter ip address: ")
-                            temp["subnetmask"] = raw_input("Enter subnet mask: ")
+                            temp["ipaddress"] = inputIp('network address') #raw_input("Enter ip address: ")
+                            temp["subnetmask"] = raw_input("Enter subnet mask: ") #inputIp('subnet mask')
                             fastethernet.append(temp)
                             '''
                         else:
@@ -451,12 +666,20 @@ def main():
                         choice4= raw_input("Press any key to add to routing table. Enter 0 to stop adding routing table entries:")
                         if(choice4 != '0'):
                             temp = {}
-                            temp["network"] = raw_input("Enter network address: ")
-                            temp["mask"] = raw_input("Enter subnet mask: ")
-                            temp["nexthop"] = raw_input("Enter next hop: ")
+                            temp["network"] = inputIpForRoutingTable('network address')
+                            temp["mask"] = raw_input("Enter subnet mask: ") #inputIp('subnet mask')
+                            temp["nexthop"] = inputIpExisting('next hop IP address')
+                            zeroFrom = len(temp['network']) #should be 15, for format xxx.xxx.xxx.xxx
+                            while zeroFrom > 0:
+                                if temp['network'][zeroFrom-1]=='0' or temp['network'][zeroFrom-1]=='.':
+                                    zeroFrom -= 1
+                                else:
+                                    break
+                            temp['zeroFrom'] = zeroFrom
                             routes.append(temp)
                         else:
                             tostop3 = 1
+                    #print(routes)
                     director.setBuilder(RouterBuilder())
                     router = director.createComponent(name = name, fastethernet = fastethernet, routes = routes)
                     adjMat["byName"][name] = router
@@ -466,7 +689,7 @@ def main():
                     linkChoice = raw_input()
                     while(linkChoice != '0'):
                         #ensure that linkchoice is valid
-                        linkChoice = raw_input('Enter name of object to connect to: ')
+                        linkChoice = checkName('Enter name of object to connect to: ') #raw_input('Enter name of object to connect to: ')
                         portNum1 = int(raw_input('Enter port number of router to connect to: '))
                         tempObj = adjMat["byName"][linkChoice]
                         if router in adjMat['byObj']:
@@ -479,7 +702,7 @@ def main():
                             adjMat['byObj'][tempObj] = [router]
 
                         #Taking care of adjMat['byIp']
-                        print(router.getFastEthernet())
+                        #print(router.getFastEthernet())
                         adjMat['byIp'][router.getFastEthernet()[portNum1-1]['ipaddress']] = tempObj
                         tempObjType = tempObj.getType()
                         if tempObjType == 'PC':
@@ -504,26 +727,46 @@ def main():
             else:
                 continue
 
+    #print('out of loop')
+    while True:
+        print('\n\n\n\nPing trial:')
+        src = inputIpExisting('source IP address') #raw_input('Enter source IP: ')
+        dst = inputIpExisting('destination IP address') #raw_input('Enter destination IP: ')
+        ping = {'sourceIP':src,'destinationIP':dst,'visitedObjs':[]}
+        if src not in adjMat['byIp'] or dst not in adjMat['byIp']:
+            print('Failed ping')
+            continue
+        #print(ping)
+        currentObjs = [adjMat['byIp'][src]]
+        #print(currentObjs)
+        pingInTransit = True
+        while pingInTransit:
+            newList = []
+            #print(currentObjs)
+            for device in currentObjs:
+                '''
+                if device == None:
+                    continue
+                '''
+                if device.receive(ping) == True:
+                    print('Successful ping')
+                    ping['status'] = True
+                    pingInTransit = False
+                    break
+                else:
+                    try:
+                        newList += device.send(ping)
+                    except KeyError:
+                        print('KeyError')
+                    #print('nl  '+str(newList))
+                    #raw_input('prompt')
+            if len(newList) == 0 and pingInTransit:
+                print('Failed ping')
+                pingInTransit = False
+            else:
+                currentObjs = newList
 
 
-    print "Router"
-    director.setBuilder(RouterBuilder())
-    router = director.createComponent(gateway='testgateway', name = 'testnamerouter')
-    router.specification()
-
-    print ""
-
-    print "PC"
-    director.setBuilder(PCBuilder())
-    pc = director.createComponent(name = 'testnamepc')
-    pc.specification()
-
-    print ""
-
-    print "Hub"
-    director.setBuilder(HubBuilder())
-    hub = director.createComponent(name = 'testnamehub')
-    hub.specification()
 
 if __name__ == "__main__":
     main()
